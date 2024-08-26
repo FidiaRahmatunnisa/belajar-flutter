6f9f96c519f9995f5ff3323975a058d3b50f2e8b
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2024-08-26 14:30:35 +0700

    materi hello world flutter

---
+++ flutter_tutor1/flutter_tutor1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("aplikasi hello world"),),
+        body: Text('hello world'),
+      ),
+    );
+  }
+}
