38c4e407a4736ac021daddda6494f53cd54eeb7f
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2024-09-06 00:22:18 +0700

    materi qrcode custom

--- qr_code/lib/main.dart
+++ qr_code/lib/main.dart
@@ -11,13 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: QrImageView(
-            data: 'This is a simple QR code',
+            data: 'https://static.promediateknologi.id/crop/8x313:725x915/0x0/webp/photo/p2/74/2024/05/07/c8b7413ccd33544ab59691a09a7b4107-2471837881.jpg',
             version: QrVersions.auto,
-            size: 320,
+            size: 250,
             gapless: false,
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.amber,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
           )
         ),
       ),
