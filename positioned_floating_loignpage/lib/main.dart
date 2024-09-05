46ad5d26281f2fd93688c0616224a49022f8cd5f
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2024-09-05 14:23:21 +0700

    materi positioned, floating action button, login page

--- positioned_floating_loignpage/lib/main.dart
+++ positioned_floating_loignpage/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
-  
+
   double getSmallDiameter(BuildContext context) =>
       MediaQuery.of(context).size.width * 2 / 3;
   double getBigDiameter(BuildContext context) =>
@@ -28,11 +32,12 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey.shade100,
       body: Stack(
         children: [
           Positioned(
-            right: - getSmallDiameter(context)/3,
-            top: - getSmallDiameter(context)/3,
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
             child: Container(
               width: getSmallDiameter(context),
               height: getBigDiameter(context),
@@ -45,13 +50,13 @@ class MainPage extends StatelessWidget {
                   ],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
-                  ),
+                ),
               ),
             ),
           ),
-           Positioned(
-            left: - getBigDiameter(context)/4,
-            top: - getBigDiameter(context)/4,
+          Positioned(
+            left: -getBigDiameter(context) / 4,
+            top: -getBigDiameter(context) / 4,
             child: Container(
               width: getBigDiameter(context),
               height: getBigDiameter(context),
@@ -64,8 +69,179 @@ class MainPage extends StatelessWidget {
                   ],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
-                  ),
+                ),
               ),
+              child: Center(
+                child: Text(
+                  'dribble',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -getBigDiameter(context) / 2,
+            bottom: -getBigDiameter(context) / 2,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.purple.shade100,
+                    Colors.pink.shade200,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.email,
+                              color: Colors.pink.shade300,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                              color: Colors.blueGrey.shade400,
+                            )),
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.pink.shade200)),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.key_rounded,
+                            color: Colors.pink.shade300,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.blueGrey.shade400),
+                          ),
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.pink.shade200),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
+                    child: Text(
+                      'FORGOT PASSWORD?',
+                      style: TextStyle(
+                          fontSize: 11, color: Colors.blueGrey.shade500),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 40,
+                        child: Container(
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(20),
+                              splashColor: Colors.amber,
+                              onTap: () {},
+                              child: Center(
+                                child: Text(
+                                  'Sign In',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                              colors: [Colors.purple, Colors.pink],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                            ),
+                          ),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {},
+                        mini: true,
+                        elevation: 0, // biar nggk ada bayangan
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: AssetImage('assets/fb.png'),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                      ),
+                      FloatingActionButton(
+                          onPressed: () {},
+                          mini: true,
+                          elevation: 0, // biar nggk ada bayangan
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: AssetImage('assets/ig.png'),
+                                  fit: BoxFit.cover),
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'DON\'T HAVE AN ACCOUNT? ',
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      ' SIGN UP',
+                      style: TextStyle(
+                          fontSize: 11,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
         ],
